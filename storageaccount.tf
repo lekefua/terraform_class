70c6524360120ca38a411ee6b1745c43c65ace64
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-18 20:32:16 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_account" "tf-test-storage" {
+resource "azurerm_storage_account" "tf-storageaccount" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.tf-test-philippe.name
   location                 = azurerm_resource_group.tf-test-philippe.location
