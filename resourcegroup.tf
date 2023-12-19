b17bdfdb7d169fc204c0e16f87c60d061e0cfd4a
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-18 19:24:08 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "tf-test-philippe" {
   name     = "mcit_resource_group_philippe"
   location = "canadacentral"
 }
-
-resource "azurerm_resource_group" "tf-test-philippe_2" {
-  name     = "mcit_resource_group_philippe_2"
-  location = "canadacentral"
-}
