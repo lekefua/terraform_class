bb00cb0974859a356cacc4a355047fdeff803fb6
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-18 20:41:20 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,11 +1,1 @@
-resource "azurerm_storage_account" "tf-storageaccount" {
-  name                     = "samcitphil2023"
-  resource_group_name      = azurerm_resource_group.tf-test-philippe.name
-  location                 = azurerm_resource_group.tf-test-philippe.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
 
-  tags = {
-    environment = "staging"
-  }
-}
