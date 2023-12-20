d77dcc8b00d5bf7f2f2ed22cfe2dc3a4918c6390
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 17:22:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,18 +14,10 @@ resource "azurerm_mssql_server" "tf-test-mysqlserver" {
 
   azuread_administrator {
     login_username = "sql-admin"
-    object_id      = "fbfaea3c-9c12-4a9c-99cd-d119744d6825"
+    object_id      =var.object_id
   }
 
   tags = {
     environment = "production"
   }
 } 
-  
-resource "azurerm_sql_active_directory_administrator" "azuread-admin" {
-  server_name         = "azurerm_sql_server.tf-test-mysqlserver.name"
-  resource_group_name = "azurerm_resource_group.tf-rg-philippe.name"
-  login               = "sql-admin"
-  tenant_id=var.tenant_id
-  object_id=var.object_id
-}
