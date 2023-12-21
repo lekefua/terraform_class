cdc518cc0a6e2ac6be2292bba779fad020ca0237
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 20:24:13 -0500

    Update localinput.tf

--- localinput.tf
+++ localinput.tf
@@ -2,7 +2,9 @@ locals {
 
   service_name = "forum"
   owner = "Community Team"
+  spare_gloves = "fourteen"
   lengthofservice_name=length(local.service_name)
   lengthofowner=length(local.owner)
+  lengthofspare_gloves=length(local.spare_gloves)
 
 }
