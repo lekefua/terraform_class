4c81506575bf7c5e7f319da4a4ee3c1b2fd09179
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 19:07:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_mssql_server" "tf-test-mysqlserver" {
   resource_group_name          = azurerm_resource_group.tf-rg-philippe.name
   location                     = azurerm_resource_group.tf-rg-philippe.location
   version                      = "12.0"
-  administrator_login          = "admin"
+  administrator_login          = var.db_user
   administrator_login_password = var.db_password
   minimum_tls_version          = "1.2"
 
