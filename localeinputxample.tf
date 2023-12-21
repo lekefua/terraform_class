0ca1ba42c5b82ed754558f62d6862b74260cbf16
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 19:59:39 -0500

    Update localeinputxample.tf

--- localeinputxample.tf
+++ localeinputxample.tf
@@ -1,8 +1,7 @@
-locals {​
+locals{​
 
   service_name = "forum"​
   owner        = "Community Team"​
   lengthofservice_name=length(local.service_name)
   lengthofowner_name=length(local.owner)
 }
-
