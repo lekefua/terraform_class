0f47ab757fd53b987f6252bfc43df8461b61063f
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 20:20:35 -0500

    Update localinput.tf

--- localinput.tf
+++ localinput.tf
@@ -3,6 +3,6 @@ locals {
   service_name = "forum"
   owner = "Community Team"
   lengthofservice_name=length(local.service_name)
-  lengthofowner=length.(local.owner)
+  lengthofowner=length(local.owner)
 
 }
