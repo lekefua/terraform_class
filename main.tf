2db675faeab60a29e3ad256b894ec81383ad985b
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 20:56:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_mssql_server" "tf-test-mysqlserver" {
 } 
 
 resource "azurerm_storage_account" "tf-storageaccount" {
-  name                     = "storageaccountname${var.countNumber}"
+  name                     = "storageaccount${var.countNumber}"
   resource_group_name      = azurerm_resource_group.tf-rg-philippe.name
   location                 = azurerm_resource_group.tf-rg-philippe.location
   account_tier             = "Standard"
