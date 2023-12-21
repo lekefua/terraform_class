5713e9ed2365ae188219abb27a89c490c72b8eac
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 20:06:03 -0500

    Update outputexample.tf

--- outputexample.tf
+++ outputexample.tf
@@ -3,17 +3,4 @@ output "print"{​
       value = local.service_name​
 }​
 
-output "secondprint"{​
-      value = local.owner​
 
-}
-
-output "thirdprint"{​
-      value = local.lengthofservice_name
-
-}
-
-output "fourthprint"{​
-      value = local.lengthofowner_name
-
-}
