670d2e2a690dc7b9712cb097989bf83b60c765ca
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-20 20:25:06 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,20 @@
 output "print"{
   value = local.service_name
+
 }
 output "secondprint"{
     value = local.owner
+
 }
 output "thirdprint"{
   value = local.lengthofservice_name
+
 }
 output "fourthprint"{
   value = local.lengthofowner
+
+}
+
+output "fifthprint"
+  value = lengthofspare_gloves
 }
