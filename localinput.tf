8c3fba231a048b1c7eba262d51d30f245649c5d9
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-22 20:45:47 -0500

    Update localinput.tf

--- localinput.tf
+++ localinput.tf
@@ -9,5 +9,9 @@ locals {
   winter_sports_list = ["hockey", "snowboarding", "ice hockey", "day drinking on a sunday"]
   canadas_best_meals = ["poutine", "smoke meat", "maple syrup", "butter chicken", "tourtiere", "sugar pie", "donair", "street meat", "steamie all dress", "kraft dinner"]
   listofnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  variousnumbers = [150, 200, 150]
+  variousnumbers = [150, 200, 150] 
+
+  cluster_names = ["k8s-yvr", "k8s-yyz", "k8s-ycg", k8s-yul"]
 }
+
+
