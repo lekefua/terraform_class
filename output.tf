b5e4f59e748f95d8f4b9c545028ab548659dafd1
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-22 19:33:45 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -35,10 +35,6 @@ output "staging_environment"{
   value = var.environment
 }
 
-output "List_of_winter_sports"{
-  value = local.winter_sports_list
-}
-
 output "loop_output"{
 	value=[for sport in local.winter_sports_list:sport]
 }
