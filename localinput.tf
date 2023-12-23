0767bf2be76e5d4eb93b93738e887d092c5805c3
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-22 20:14:31 -0500

    Update localinput.tf

--- localinput.tf
+++ localinput.tf
@@ -9,4 +9,5 @@ locals {
   winter_sports_list = ["hockey", "snowboarding", "ice hockey", "day drinking on a sunday"]
   canadas_best_meals = ["poutine", "smoke meat", "maple syrup", "butter chicken", "tourtiere", "sugar pie", "donair", "street meat", "steamie all dress", "kraft dinner"]
   listofnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  variousnumbers = [150, 200, 150]
 }
