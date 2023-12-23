04f25a688993a0702e58d1b6d5c02c5ff130bbdc
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2023-12-22 19:21:30 -0500

    Update localinput.tf

--- localinput.tf
+++ localinput.tf
@@ -6,6 +6,6 @@ locals {
   lengthofservice_name=length(local.service_name)
   lengthofowner=length(local.owner)
   lengthofspare_gloves=length(local.spare_gloves)
-  winter_sports_list=["hockey", "snowboarding", "ice hockey", "day drinking on a sunday"]
+  winter_sports_list = ["hockey", "snowboarding", "ice hockey", "day drinking on a sunday"]
 
 }
