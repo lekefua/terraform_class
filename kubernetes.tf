ede9f9bbbfadfd89fc53416ad2bfb45fb4c9e5d7
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-05 20:09:33 -0500

    Create kubernetes.tf

---
+++ kubernetes.tf
@@ -0,0 +1,26 @@
+locals {
+name_of_storage_acc_list = ["philippemcitredston1", "philippemcitredston2","philippemcitredston3", "philippemcitredston4", "philippemcitredston5"]
+}
+
+
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  for_each            = {for cluster in local.name_of_storage_acc_list: cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.tf-rg-philippe.location
+  resource_group_name = azurerm_resource_group.tf-rg-philippe.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
