9ac694f7ed85bb3b6d22bdd263f343479a494c64
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-05 20:08:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,24 +3,6 @@ resource "azurerm_resource_group" "tf-rg-philippe" {
   location = "canadacentral"
 }
 
-resource "azurerm_mssql_server" "tf-test-mysqlserver" {
-  name                         = "tftestsqlserver"
-  resource_group_name          = azurerm_resource_group.tf-rg-philippe.name
-  location                     = azurerm_resource_group.tf-rg-philippe.location
-  version                      = "12.0"
-  administrator_login          = var.db_user
-  administrator_login_password = var.db_password
-  minimum_tls_version          = "1.2"
-
-  azuread_administrator {
-    login_username = "sql-admin"
-    object_id      =var.object_id
-  }
-
-  tags = {
-    environment = "production"
-  }
-
 locals {
 name_of_storage_acc_list = ["philippemcitredston1", "philippemcitredston2","philippemcitredston3", "philippemcitredston4", "philippemcitredston5"]
 }
