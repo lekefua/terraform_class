45e16cb715d2a66b55e1c180143d589fd88f0569
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-05 20:12:35 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,1 +1,18 @@
 
+resource "azurerm_resource_group" "tf-rg-philippe"
+
+resource "azurerm_storage_account" "tf-storageaccount" {
+ // count                 = length(var.Storage_list)
+  // name                  = var.Storage_list[count.index].name
+  for_each                 = toset(var.Storage_list)
+  name                     =each.value
+  resource_group_name      = azurerm_resource_group.tf-rg-philippe.name
+  location                 = azurerm_resource_group.tf-rg-philippe.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  cross_tenant_replication_enabled =var.cross_tenant_replication
+
+  tags = {
+    environment = "staging"
+  }
+}
