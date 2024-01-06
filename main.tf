b600a4285b683e782ec95d0f4db6c7787b26e406
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-05 19:59:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,9 +23,9 @@ resource "azurerm_mssql_server" "tf-test-mysqlserver" {
 } 
 
 locals {
-name_of_storage_acc_list = = ["philippemcitredston1", "philippemcitredston2","philippemcitredston3", "philippemcitredston4", "philippemcitredston5" ]}
+name_of_storage_acc_list = ["philippemcitredston1", "philippemcitredston2","philippemcitredston3", "philippemcitredston4", "philippemcitredston5" ]}
 
-resource "azurerm_storage_account" "local-input-storage" {
+resource "azurerm_kubernetes_cluster" "batchabcd" {
   for_each            = {for cluster in local.name_of_storage_acc_list: cluster=>cluster}
   name                = "${var.prefix}cluster"
   location            = azurerm_resource_group.tf-rg-philippe.location
@@ -33,18 +33,4 @@ resource "azurerm_storage_account" "local-input-storage" {
   dns_prefix          = "exampleaks1"
 }
 
-resource "azurerm_storage_account" "tf-storageaccount" {
- // count                 = length(var.Storage_list)
-  // name                  = var.Storage_list[count.index].name
-  for_each                 = toset(var.Storage_list)
-  name                     =each.value
-  resource_group_name      = azurerm_resource_group.tf-rg-philippe.name
-  location                 = azurerm_resource_group.tf-rg-philippe.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  cross_tenant_replication_enabled =var.cross_tenant_replication
 
-  tags = {
-    environment = "staging"
-  }
-}
